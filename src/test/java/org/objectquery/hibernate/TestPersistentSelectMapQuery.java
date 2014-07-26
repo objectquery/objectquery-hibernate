9c57b8d4a96e05755b4946b4713cadf4d5cea99a
tglman <tglman@tglman.com>
2014-07-26 16:56:11 +0100

    add test for multiple and nested mapping.

--- src/test/java/org/objectquery/hibernate/TestPersistentSelectMapQuery.java
+++ src/test/java/org/objectquery/hibernate/TestPersistentSelectMapQuery.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.objectquery.SelectMapQuery;
 import org.objectquery.generic.GenericSelectQuery;
+import org.objectquery.generic.ObjectQueryException;
 import org.objectquery.hibernate.domain.Person;
 import org.objectquery.hibernate.domain.PersonDTO;
 
@@ -32,6 +33,41 @@ public class TestPersistentSelectMapQuery {
 		List<PersonDTO> res = HibernateObjectQuery.execute(query, session);
 		assertThat(res.size(), CoreMatchers.is(1));
 		assertThat(res.get(0).getName(), CoreMatchers.is("tom"));
+	}
+
+	@Test
+	public void testSimpleSelectMapNoFilter() {
+		SelectMapQuery<Person, PersonDTO> query = new GenericSelectQuery<Person, PersonDTO>(Person.class, PersonDTO.class);
+		query.prj(query.target().getName(), query.mapper().getName());
+
+		List<PersonDTO> res = HibernateObjectQuery.execute(query, session);
+		assertThat(res.size(), CoreMatchers.is(3));
+		for (PersonDTO personDTO : res) {
+			assertThat(personDTO.getName(), CoreMatchers.notNullValue());
+		}
+	}
+
+	@Test
+	public void testSimpleSelectMapTwoEntries() {
+		SelectMapQuery<Person, PersonDTO> query = new GenericSelectQuery<Person, PersonDTO>(Person.class, PersonDTO.class);
+		query.eq(query.target().getName(), "tom");
+		query.prj(query.target().getName(), query.mapper().getName());
+		query.prj(query.target().getDog().getName(), query.mapper().getSurname());
+
+		List<PersonDTO> res = HibernateObjectQuery.execute(query, session);
+		assertThat(res.size(), CoreMatchers.is(1));
+		assertThat(res.get(0).getName(), CoreMatchers.is("tom"));
+		assertThat(res.get(0).getSurname(), CoreMatchers.is("cerberus"));
+	}
+
+	@Test(expected = ObjectQueryException.class)
+	public void testSimpleSelectMapDeep() {
+		SelectMapQuery<Person, PersonDTO> query = new GenericSelectQuery<Person, PersonDTO>(Person.class, PersonDTO.class);
+		query.eq(query.target().getName(), "tom");
+		query.prj(query.target().getName(), query.mapper().getName());
+		query.prj(query.target().getHome().getAddress(), query.mapper().getAddressDTO().getStreet());
+
+		HibernateObjectQuery.execute(query, session);
 	}
 
 	@After
